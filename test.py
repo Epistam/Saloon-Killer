8147559c8ccbc614c9eaec22d039ff4e03307475
Titou <titou1384@gmail.com>
2016-05-14 17:10:59 +0200

    Code cleaned, added verbose commentary, add background rescale & temporary tempo to make the window appear

--- test.py
+++ test.py
@@ -1,35 +1,45 @@
-import pygame
-import os
-from pygame.locals import *
+# créer header
 
+# Imports
+import os, sys
+import pygame
+import time #temp
+# from pygame.locals import *
+
+# Initialisation de Pygame
 pygame.init()
 
-#ouverture de la fenetre pygame
-fenetre = pygame.display.set_mode((1000, 667))
+# Création de la fenêtre (pas de résolution spécifiée, le jeu s'adapte à tous les écrans et en tire la résolution maximale)
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption("Saloon Killer")
+displayInfo = pygame.display.Info() # On récupère les informations sur le display et notamment la résolution choisie
 
-#Chargement et collage du fond
-fond = pygame.image.load("img/FondSaloon.jpg")
-fenetre.blit(fond, (0,0))
+# Initialisation de l'arrière-plan
+background = pygame.image.load("img/background.png") # Chargement de l'image
+# On étire l'image à la taille de l'écran en récupérant les attributs nécessaires de displayInfo
+background = pygame.transform.scale(background, (displayInfo.current_w, displayInfo.current_h))
+window.blit(background, (0,0)) # Et on la colle à la surface
 
 #Chargement et collage du personnage
+#
+#
+#Barman = pygame.image.load("BarmanT.png").convert_alpha()
+#fenetre.blit(Barman, (500,320))
 
-
-Barman = pygame.image.load("BarmanT.png").convert_alpha()
-fenetre.blit(Barman, (500,320))
-
-texte = pygame.image.load("PBarman.png")
+#texte = pygame.image.load("PBarman.png")
 
 
 #Rafraichissement de l'ecran
 pygame.display.flip()
 
-game_over = False
+# game_over = False
 
-while not game_over:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			game_over = True
+# while not game_over:
+# 	for event in pygame.event.get():
+# 		if event.type == pygame.QUIT:
+# 			game_over = True
+
+time.sleep(5)
 			
 pygame.quit()
 quit()
